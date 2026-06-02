872b6345913c5caa06f4777ee9c5ee7aa6f7fef0
Hồ Minh Duy <duyhm.ce190378@gmail.com>
2026-06-02 10:20:45 +0000

    feat: initialize movie theater frontend with React and Vite

---
+++ movie-theater-be/src/test/java/com/thdpv/movietheater/MovieTheaterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thdpv.movietheater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTheaterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
