6e7c209286d2876c1a490d36459fc5da81f7e198
minhduyhodev <duyhm.ce190378@gmail.com>
2026-06-11 10:07:10 +0700

    fix(vite.config): remove merge conflict markers and clean up configuration

--- movie-theater-fe/vite.config.js
+++ movie-theater-fe/vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -35,12 +34,3 @@ export default defineConfig({
     sourcemap: false,
   },
 });
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> a4bac7dce1b3d5f10bd6d4c46c00bc60cac68786
